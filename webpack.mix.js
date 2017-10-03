abe37abb24dd9a67d38684d78d68b46f13e11439
George Hanson <georgehanson@gmlconsulting.co.uk>
2017-10-03 14:21:00 +0100

    Update build

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,4 @@ let mix = require('laravel-mix');
 
 mix.setPublicPath('dist');
 mix.ts('src/index.ts', 'dist/build.js');
-mix.ts('index.ts', 'dist/index.js');
 mix.sass('src/scss/toastify.scss', 'dist/toastify.css');
